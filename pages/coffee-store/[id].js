4c740964fdd834b1b063ee82f695419158f16d32
Nazmul Hasan Nannu <nazmulnannu43@gmail.com>
2023-02-10 23:43:28 +0600

    pending

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,10 +1,12 @@
 import fetchCoffeStoresData from "@/lib/coffe-stores";
+import { StoreContext } from "@/store/store-context";
+import { isEmpty } from "@/utils";
 import cls from "classnames";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styles from "../../styles/coffee-store.module.css";
 
 export async function getStaticProps(staticProps) {
@@ -37,11 +39,58 @@ export async function getStaticPaths() {
 }
 
 const CoffeeStore = (props) => {
+  const [coffeeStore, setCoffeeStore] = useState(props.coffeeStore || {});
+
+  const {
+    state: { coffeeStores },
+  } = useContext(StoreContext);
+
   const router = useRouter();
+  const id = router.query.id;
+
+  useEffect(() => {
+    if (isEmpty(props.coffeeStore)) {
+      if (coffeeStores.length > 0) {
+        const findCoffeeStoreById = coffeeStores.find((coffeeStore) => {
+          return coffeeStore.id.toString() === id; //dynamic id
+        });
+        setCoffeeStore(findCoffeeStoreById);
+        handleCoffeStores(findCoffeeStoreById);
+      }
+    } else {
+      // SSG
+      // handleCoffeStores(initialProps.coffeeStore);
+    }
+
+    //eslint-disable-next-line
+  }, [id]);
 
   if (router.isFallback) {
     return <div>Loading...</div>;
   }
+
+  const handleCoffeStores = async () => {
+    try {
+      const { id, name, voting, imgUrl, country, address } = coffeeStore;
+      const response = await fetch("/api/createCoffeStore", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id,
+          name,
+          voting: 0,
+          imgUrl,
+          country: country || "",
+          address: address || "",
+        }),
+      });
+      const dbCoffeeStore = await response.json();
+    } catch (error) {
+      console.error("Error creating coffee store", err);
+    }
+  };
 
   const {
     name,
